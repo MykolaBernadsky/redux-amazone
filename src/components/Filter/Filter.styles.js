93327e881c158f91662957f725fc47c846e30a0c
Mykola Bernadsky <37669136+MykolaBernadsky@users.noreply.github.com>
2020-11-23 00:09:02 +0200

    add filter bar layout + logic

---
+++ src/components/Filter/Filter.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem;
+  margin: 1rem;
+  border-bottom: .1rem #c0c0c0 solid;
+
+`;
+
+export const FilterResult = styled.div`
+ 
+`;
+
+export const FilterSort = styled.div`
+ 
+`;
+
+export const FilterSize = styled.div`
+ 
+`;
